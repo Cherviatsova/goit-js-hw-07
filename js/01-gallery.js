3bef1a41810281e9459c3a60ce87ff33075bea88
Cherviatsova <chervyatsovaelena2@gmail.com>
2022-01-04 17:19:29 +0200

    fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const galleryImages = createGalleryImagesMarkup(galleryItems);
 
-
 function createGalleryImagesMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
@@ -16,34 +16,33 @@ function createGalleryImagesMarkup(galleryItems) {
       alt="${description}"
     />
   </a>
-</div>`
-    }).join('')
-};
-gallery.insertAdjacentHTML('beforeend', galleryImages)
-
+</div>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", galleryImages);
 
 let originalImage;
 
-gallery.addEventListener('click', event => {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    originalImage = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  originalImage = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
 
-    originalImage.show()
+  originalImage.show();
 });
 
-
- gallery.addEventListener('keydown', onEscKeyPress)
+gallery.addEventListener("keydown", onEscKeyPress);
 
 function onEscKeyPress(event) {
-    if (event.code !== 'Escape') {
-        return;
-    }
-    originalImage.close()
-};
+  if (event.code !== "Escape") {
+    return;
+  }
+  originalImage.close();
+}
 
 console.log(galleryItems);
